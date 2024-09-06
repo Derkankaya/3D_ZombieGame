4dca315627578c7c4316383428d66baa19e57ac5
Derkankaya <114098020+Derkankaya@users.noreply.github.com>
2024-09-06 19:02:03 +0300

    part 3

---
+++ Assets/zombie/z_script/z_script.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class z_script : MonoBehaviour
+{
+    private float zombieHP = 100;
+    bool zombieDead;
+    Animator zombieAnim;
+
+    GameObject hedefOyuncu;
+    public float rangePlayer;
+    public float rangeEnemy;
+    NavMeshAgent zombieNavMesh;
+
+    void Start()
+    {
+        zombieAnim = this.GetComponent<Animator>();
+        hedefOyuncu = GameObject.Find("character");
+        zombieNavMesh = this.GetComponent<NavMeshAgent>();
+
+    }
+
+    
+    void Update()
+    {
+        
+        if (zombieHP <= 0)
+        {
+            zombieDead = true;
+        }
+        if (zombieDead == true)
+        {
+            zombieAnim.SetBool("Dead", true);
+            StartCoroutine(YokOl());
+        }
+        else
+        {
+           float range = Vector3.Distance(this.transform.position, hedefOyuncu.transform.position);
+            if (range < rangePlayer) {
+                zombieNavMesh.isStopped = false;
+                zombieNavMesh.SetDestination(hedefOyuncu.transform.position);
+                zombieAnim.SetBool("Run", true);
+                zombieAnim.SetBool("Strike", false);
+                this.transform.LookAt(hedefOyuncu.transform.position);
+            }
+            else
+            {
+                zombieNavMesh.isStopped = true;
+                zombieAnim.SetBool("Run", false);
+                zombieAnim.SetBool("Strike", false);
+            }
+            if(range<rangeEnemy)
+            {
+                zombieNavMesh.isStopped = true;
+                zombieAnim.SetBool("Run", false);
+                zombieAnim.SetBool("Strike", true);
+                this.transform.LookAt(hedefOyuncu.transform.position);
+
+            }
+        }
+    }
+    public void inflictDamage()
+    {
+        hedefOyuncu.GetComponent<c_script>().TakeDamage();
+
+    }
+    IEnumerator YokOl()
+    {
+        yield return new WaitForSeconds(10);
+        Destroy(this.gameObject);
+    }
+
+    public void HasarAl()
+    {
+        zombieHP -= Random.Range(25f, 35f);
+    }
+}
